ad96379cd6a736d5e563106030d8373bb09cc9f0
Henri <henrikelder@gmail.com>
2018-04-02 12:22:18 +0300

    Code polish

--- Lesson1/factors.py
+++ Lesson1/factors.py
@@ -1,10 +1,12 @@
 num = int(input("Enter a number: "))
 
+
 def factors(x):
 
-	print("The factors of",x,"are:")
-	for i in range(1, x + 1):
-        	if x % i == 0:
-            		print(i)
+    print("The factors of", x, "are:")
+    for i in range(1, x + 1):
+            if x % i == 0:
+                print(i)
+
 
 factors(num)
--- Lesson1/powers.py
+++ Lesson1/powers.py
@@ -1,4 +1,4 @@
 n = int(input("Enter a number:"))
 exponent = 0
 for exponent in range(17):
-	print(n ** exponent)
+    print(n ** exponent)
