7b555ebbf58f882ab535cf6ab0ee81d3ba146cda
Henri <henrikelder@gmail.com>
2018-04-02 09:30:22 +0300

    Class exercises

---
+++ Lesson1/lesson1.py
@@ -0,0 +1,20 @@
+#name = input("Enter your name:")
+#print('My name is', name)
+
+'''ADDRESS_NUMBER = 15
+ADDRESS = 'Tallinn'
+print('My address is', ADDRESS, ADDRESS_NUMBER)'''
+
+'''a = 5
+A = 15
+
+if a is A:
+	print("True")
+else:
+	print("False")'''
+
+a = 10
+b = 20
+def sum():
+	print(a + b)
+sum()
