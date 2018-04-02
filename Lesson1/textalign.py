a46e44e1dc1a7ff71f4bd57e06ef7ca8c24ae472
Henri <henrikelder@gmail.com>
2018-04-02 12:25:58 +0300

    .

--- Lesson1/textalign.py
+++ Lesson1/textalign.py
@@ -1,6 +1,6 @@
 print("Please make sure that the width is at least 11, for a pleasant table.")
 width = int(input("Enter the width:"))
-with open(r'C:\Users\henri\Desktop\Python\Lesson1\info.txt', 'r') as info:
+with open(r'.\info.txt', 'r') as info:
     for line in info:
         data = line.split()
         print('{0[0]:>{width}}{0[1]:>{width}}{0[2]:>12}'.format(data, width=width))
