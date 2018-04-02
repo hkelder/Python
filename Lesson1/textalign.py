7ebac46b585633205d4dbf4f233c67e508cbaaac
Henri <henrikelder@gmail.com>
2018-04-02 12:23:12 +0300

    Finished textalign exercise, with a little cheat

--- Lesson1/textalign.py
+++ Lesson1/textalign.py
@@ -1,2 +1,6 @@
-str = "This is string textalign example.";
-print str.rjust(20, '0')
+print("Please make sure that the width is at least 11, for a pleasant table.")
+width = int(input("Enter the width:"))
+with open(r'C:\Users\henri\Desktop\Python\Lesson1\info.txt', 'r') as info:
+    for line in info:
+        data = line.split()
+        print('{0[0]:>{width}}{0[1]:>{width}}{0[2]:>12}'.format(data, width=width))
