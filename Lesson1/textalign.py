7261c8b7347709c582ad8db9a4799a97e29097c3
Henri <henrikelder@gmail.com>
2018-04-03 20:40:28 +0300

    Added ValueError for min. req. input

--- Lesson1/textalign.py
+++ Lesson1/textalign.py
@@ -1,5 +1,8 @@
-print("Please make sure that the width is at least 11, for a pleasant table.")
+print("Please make sure that the width is at least 10, for a pleasant table.")
 width = int(input("Enter the width:"))
+if width < 10:
+    raise ValueError('Please enter a number bigger or equal to 10')
+
 with open(r'.\info.txt', 'r') as info:
     for line in info:
         data = line.split()
