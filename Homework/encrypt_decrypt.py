1c7bc85fed924e29b9146b84107add47d40b9182
Henri <henrikelder@gmail.com>
2018-04-07 18:30:54 +0300

    Finished homework with code rework

--- Homework/encrypt_decrypt.py
+++ Homework/encrypt_decrypt.py
@@ -3,53 +3,63 @@
 # 1. Write the program to encrypt the text file (case insensitive) into cipher text
 # 2. Write the program to decrypt the cipher text to plain text(normal text- case insensitive)
 
-plainText = input("Enter a some text:")
-shift = 5
+MAX_SHIFT_SIZE = 26
 
 
-def caesar(plainText, shift):
-    """Encrypt inputted text via Caesar cipher (shift by 5).
+def decryptionMode():
+    while True:
+        print("Do you wish to encrypt or decrypt?")
+        mode = input().lower()
+        if mode in ['encrypt', 'e', 'decrypt', 'd']:
+            return mode
+        else:
+            print("Enter one of the following: encrypt, e, decrypt, d")
 
-    Keyword arguments:
-    plainText -- user inputted text
-    shift -- 5
 
-    returns string
+def userMessage():
+    print("Enter your message:")
+    return input()
 
-    """
 
-    cipherText = ""
-    for character in plainText:
+def caesarShift():
+    shift = 0
+    while True:
+        print("Enter the shift number (1-%s)" % MAX_SHIFT_SIZE)
+        shift = int(input())
+        if shift >= 1 & shift <= MAX_SHIFT_SIZE:
+            return shift
+
+
+def caesarsMessage(mode, message, shift):
+    if mode[0] == 'd':
+        shift = -shift
+    translated = ''
+
+    for character in message:
         if character.isalpha():
-            alphabet = ord(character) + shift
-            if alphabet > ord('z'):
-                alphabet -= 26
-            finalLetter = chr(alphabet)
-            cipherText += finalLetter
+            asciiNumber = ord(character)
+            asciiNumber += shift
 
-    print(cipherText)
-    return cipherText
+            if character.isupper():
+                if asciiNumber > ord('Z'):
+                    asciiNumber -= 26
+                elif asciiNumber < ord('A'):
+                    asciiNumber += 26
+            elif character.islower():
+                if asciiNumber > ord('z'):
+                    asciiNumber -= 26
+                elif asciiNumber < ord('a'):
+                    asciiNumber += 26
 
-
-caesar(plainText, shift)
-
-
-def decryption():
-
-    encryptedText = input("Enter your encrypted text:")
-    encryptionShift = 5
-
-    encrypText = ""
-    for character in encryptedText:
-        if character.isalpha():
-            alphabet = ord(character) - encryptionShift
-        if alphabet > ord('z'):
-            alphabet += 26
-        finalLetter = chr(alphabet)
-        encrypText += finalLetter
-
-    print(encrypText)
-    return encrypText
+            translated += chr(asciiNumber)
+        else:
+            translated += character
+    return translated
 
 
-decryption()
+mode = decryptionMode()
+message = userMessage()
+shift = caesarShift()
+
+print("Your translated text is:")
+print(caesarsMessage(mode, message, shift))
