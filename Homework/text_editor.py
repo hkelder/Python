3a5c29228adfea76d9b475debcd8d234aee69edb
Henri <henrikelder@gmail.com>
2018-04-29 14:49:23 +0300

    Assignment 3

--- Homework/text_editor.py
+++ Homework/text_editor.py
@@ -8,11 +8,66 @@
 #
 # Help Menu: upon clicking it should generate a message box to give brief info.
 #  e.g. version of the application, probably author name....
-
+import random
 import tkinter
-from tkinter import *
+from tkinter.filedialog import *
+from tkinter.messagebox import *
 
-screen = Tk()
+Screen = tkinter.Tk()
+Screen.title("Assignment 3")
+Screen.geometry()
+Area = Text(Screen)
+TextFile = None
 
 
-screen.mainloop()
+def open_file():
+    TextFileTemporary = askopenfilename()
+    print(TextFileTemporary)
+
+    if TextFileTemporary:
+        TextFile = TextFileTemporary
+        Area.grid()
+        Area.delete(1.0, END)
+        file = open(TextFile, "r")
+        Area.insert(1.0, file.read())
+        file.close()
+        print(TextFile)
+
+
+def save_file():
+    file = open(TextFile, "w")
+    file.write(Area.get(1.0, END))
+    file.close()
+
+
+def close_editor():
+    Screen.destroy()
+
+
+def help_info():
+    showinfo("Text Editor 1.0", "Henri Kelder\nhekeld@ttu.ee")
+
+
+def mordor():
+    text = """Three Rings for the Elven-kings under the sky,
+Seven for the Dwarf-lords in their halls of stone,
+Nine for Mortal Men doomed to die,
+One for the Dark Lord on his dark throne
+In the Land of Mordor where the Shadows lie.
+One Ring to rule them all, One Ring to find them,
+One Ring to bring them all and in the darkness bind them,
+In the Land of Mordor where the Shadows lie."""
+    showinfo("Rings of Power", text)
+
+
+MenuBar = Menu(Screen)
+FileMenu = Menu(MenuBar, tearoff=0)
+Screen.config(menu=MenuBar)
+MenuBar.add_cascade(label="File", menu=FileMenu)
+MenuBar.add_command(label="Help", command=help_info)
+MenuBar.add_command(label="???", command=mordor)
+FileMenu.add_command(label="Open", command=open_file)
+FileMenu.add_command(label="Save", command=save_file)
+FileMenu.add_command(label="Exit", command=close_editor)
+
+Screen.mainloop()
