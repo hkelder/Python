1ee43873fa83a991581e228d07b9616ad5bbab52
Henri <henrikelder@gmail.com>
2018-04-29 13:50:42 +0300

    Assignment 3 beginning

---
+++ Homework/text_editor.py
@@ -0,0 +1,18 @@
+# Write a program (GUI based) which does the same job as Notepad (Windows' application).
+#
+# It must have a menu Bar and two menus File menu and Help menu.
+# Inside the File menu, there should be 3 functions, Open, Save and Close
+# 1. Open, which lets user to chose a text file to display in the text area.
+# 2. Save, should save the contents of text area in a text file.
+# 3. Close, should terminate the application.
+#
+# Help Menu: upon clicking it should generate a message box to give brief info.
+#  e.g. version of the application, probably author name....
+
+import tkinter
+from tkinter import *
+
+screen = Tk()
+
+
+screen.mainloop()
