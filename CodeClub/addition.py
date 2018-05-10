f588deb8e96fbb1de19a0a65aa5238bd1f11ce11
Henri <henrikelder@gmail.com>
2018-05-10 11:02:40 +0300

    Challenge descriptions

--- CodeClub/addition.py
+++ CodeClub/addition.py
@@ -1,2 +1,3 @@
+# Write a function add(number1, number2) that returns the sum of the two numbers
 def add(number1, number2):
     return number1 + number2
--- CodeClub/hello.py
+++ CodeClub/hello.py
@@ -1,3 +1,4 @@
+# Write a function hello() that takes at least one argument - someone’s name.
 def hello(name):
     output = "Hello " + name
     return output
