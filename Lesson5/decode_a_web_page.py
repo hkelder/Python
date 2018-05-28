e59b5cb0cbc9083b1d45f351fcaf4d27ff2918c8
Henri <henrikelder@gmail.com>
2018-05-28 20:22:32 +0300

    Output formatting

--- Lesson5/decode_a_web_page.py
+++ Lesson5/decode_a_web_page.py
@@ -3,7 +3,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-print("This program will take New York Times html and print out all article titles.")
+print("This program will take New York Times html and print out all article titles. \n")
 
 url = "https://www.nytimes.com/"
 r = requests.get(url)
@@ -11,6 +11,6 @@ soup = BeautifulSoup(r.text, 'html.parser')
 
 for story_heading in soup.find_all(class_='story-heading'):
     if story_heading.a:
-        print(story_heading.a.text + '\n')
+        print(story_heading.a.text.replace('\n', ' ').strip() + '\n')
     else:
-        print(story_heading.text + '\n')
+        print(story_heading.contents[0].strip() + '\n')
