c5080d8c3ed0bbc33da1ff0a7764b1e183c08e3a
Henri <henrikelder@gmail.com>
2018-05-28 20:15:52 +0300

    Webpage html decoding

---
+++ Lesson5/decode_a_web_page.py
@@ -0,0 +1,16 @@
+# Use the BeautifulSoup and requests Python packages to print out a list of all the article titles on the New York Times
+
+import requests
+from bs4 import BeautifulSoup
+
+print("This program will take New York Times html and print out all article titles.")
+
+url = "https://www.nytimes.com/"
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'html.parser')
+
+for story_heading in soup.find_all(class_='story-heading'):
+    if story_heading.a:
+        print(story_heading.a.text + '\n')
+    else:
+        print(story_heading.text + '\n')
